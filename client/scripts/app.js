07b2af81f0904b32aa26369b5dd3e276a55d6fc5
dierat <diedra.rater@gmail.com>
2015-07-14 20:36:22 -0700

    got send message functionality working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,7 @@
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
   timeStamp: new Date("October 13, 1976 11:13:00")
+  //add room array as a property
 };
 
 // Use the browser's built-in functionality to quickly and safely escape the
@@ -13,16 +14,19 @@ var escapeHtml = function (str) {
     return div.innerHTML;
 };
  
-// UNSAFE with unsafe strings; only use on previously-escaped ones!
-var unescapeHtml = function (escapedStr) {
-    var div = document.createElement('div');
-    div.innerHTML = escapedStr;
-    var child = div.childNodes[0];
-    return child ? child.nodeValue : '';
+// makes a new room?
+app.init = function(){
+  var url = window.location.href;
+  for (var i = url.length - 1; i >= 0; i--) {
+    if (url[i]==='='){
+      app.username = url.slice(i+1);
+    }
+  }
+  console.log(app.username);
+  app.fetch();
 };
 
-// makes a new room?
-app.init = function(){};
+
 
 app.send = function(data){
   $.ajax({
@@ -41,42 +45,60 @@ app.send = function(data){
   });
 };
 
+
+
 app.fetch = function(){
 
   var readyState = $.get('https://api.parse.com/1/classes/chatterbox', function(data, status){
 
     app.messages = data.results;
 
-    // console.log("data = ", data);
-    return data.results;
+    app.addMessage();
   });
 };
 
-app.clearMessages = function(){};
 
-app.addMessage = function(){
 
-  console.log("app.messages = ", app.messages);
-//need to get dates to compare with each other
+app.clearMessages = function(){
+  $('.allMessages').html('');
+  app.timeStamp = new Date("October 13, 1976 11:13:00");
+};
 
-// their date comes in this format: 2015-07-14T03:16:38.685Z
+
+app.filterMessages = function(room){
+  var messages = app.messages.reverse();
+  
+  _.filter(messages, function(obj){
+    var dateString = new Date(obj.createdAt);
+    return dateString > app.timeStamp;
+  });
+
+  // filter by room if defined
+};
+
+
+
+app.addMessage = function(messagesArray){
+  console.log("trying to add new messages");
+
+
 
   for (var i = 0; i < app.messages.length; i++) {
-    var dateString = new Date(app.messages[i].createdAt);
-    if (dateString > app.timeStamp){
-      
-      var message = $('.allMessages').append('<div class = "message">');
+    console.log("found a new message!");
+    
+    var message = $('<div class = "message" />');
+    $('.allMessages').prepend(message);
 
-      var userName = '@' + escapeHtml( app.messages[i].username );
-      userName = $(('<span class = "userName">' + userName + '</span>'));
-      var dateCreated = escapeHtml( app.messages[i].createdAt );
-      dateCreated = $(('<span class = "dateCreated">'+ dateCreated +'</span>'));
-      var updatedAt = escapeHtml( app.messages[i].updatedAt );
-      updatedAt = $(('<span class = "updatedAt">' + updatedAt +'</span>'));
-      var text = escapeHtml( app.messages[i].text );
-      text = $(('<span class = "text">' + text + '</span>'));
-      message.append(userName, userName, dateCreated,updatedAt,text);
-    }
+    var userName = '@' + escapeHtml( app.messages[i].username );
+    userName = $(('<span class = "userName">' + userName + '</span>'));
+    var dateCreated = escapeHtml( app.messages[i].createdAt );
+    dateCreated = $(('<span class = "dateCreated">'+ dateCreated +'</span>'));
+    var updatedAt = escapeHtml( app.messages[i].updatedAt );
+    updatedAt = $(('<span class = "updatedAt">' + updatedAt +'</span>'));
+    var text = escapeHtml( app.messages[i].text );
+    text = $(('<span class = "text">' + text + '</span>'));
+
+    message.append(userName, userName, dateCreated, updatedAt, text);
   }
 
   app.timeStamp = new Date();
@@ -84,13 +106,44 @@ app.addMessage = function(){
 
 app.addRoom = function(){};
 
+app.init();
+
 setInterval(function(){
   app.fetch();
 }, 1500);
 
 
 
+// app.send(message);
+$(document).ready(function(){
+  $("#messageSubmission").on('submit', function(event){
+    var msg = $(".message").val();
+    $(".message").val('');
+    
+    var message = {
+      username: app.username,
+      text : msg,
+      roomname: 'lobby'
+    };
+    app.send(message);
 
-// var $newdiv1 = $( "<div id='object1'/>" )
+    event.preventDefault();
+  });
+});
 
-// var $tweet = $('<div class="tweet"><span class="user">@' + user + '</span> <span class="time">(' + time + ')</span>: <span class="message">' + message + '</span></div>');
+/*
+
+$('#submitButton').on('click', function(){
+
+  var msg = $("#messageField").val();
+
+  var twt = {user: usr, message: msg, created_at: new Date()};
+
+  addTweet(twt);
+  loadTweets();
+
+  $('#messageField').val('');
+
+});
+
+*/
